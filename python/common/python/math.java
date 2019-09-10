24b288c1276e17c4377ba08f74537e871ae0da74
Michael Rehn <rehn.michael@outlook.com>
2019-09-10 15:47:02 +0200

    Code style changes to fix requested changes

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -9,9 +9,6 @@ public class math extends org.python.types.Module {
         super();
     }
 
-    private static long vm_start_time;
-    public static org.python.Object _STRUCT_TM_ITEMS;
-
     @org.python.Attribute
     public static org.python.Object __file__ = new org.python.types.Str("python/common/python/math.java");
     @org.python.Attribute
@@ -22,7 +19,6 @@ public class math extends org.python.types.Module {
     public static org.python.Object __package__ = new org.python.types.Str("");
     @org.python.Attribute
     public static org.python.Object __spec__ = org.python.types.NoneType.NONE;  // TODO
-    public static org.python.types.Int altzone;
 
     // Python has a maximum value of float larger than javas. This is pythons max float
     public static double pythonMaxFloat = 1.7976931348623157e+308;
@@ -34,14 +30,12 @@ public class math extends org.python.types.Module {
     public static org.python.Object exp(org.python.Object x)
         throws org.python.exceptions.OverflowError {
         // The float type uses double internally, so we can simply use that
-        double x_double = ((org.python.types.Float) x.__float__()).value;
-        double result = Math.exp(x_double);
+        double xValue = ((org.python.types.Float) x.__float__()).value;
+        double result = Math.exp(xValue);
         if (result > pythonMaxFloat) {
             throw new org.python.exceptions.OverflowError("math range error");
         } else {
             return new org.python.types.Float(result);
         }
     }
-
 }
-
--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -1,8 +1,5 @@
-import unittest
-
 from unittest import expectedFailure
 from ..utils import TranspileTestCase
-
 
 class MathModuleTests(TranspileTestCase):
     def test_sqrt(self):
@@ -32,23 +29,27 @@ class MathModuleTests(TranspileTestCase):
     @expectedFailure
     def test_exp_fail_nan(self):
         self.assertCodeExecution("""
+            import math
             print(math.exp('this is not a number'))
         """)
 
     @expectedFailure
     def test_exp_fail_two_args(self):
         self.assertCodeExecution("""
+            import math
             print(math.exp(1, 2))
         """)
 
     @expectedFailure
     def test_exp_fail_array_arg(self):
         self.assertCodeExecution("""
+            import math
             print(math.exp([1, 2, 3]))
         """)
 
     @expectedFailure
     def test_exp_fail_large(self):
         self.assertCodeExecution("""
+            import math
             print(math.exp(100000))
         """)
