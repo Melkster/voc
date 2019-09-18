caee1ae1e102ae9231959f7e1edd7c3af0ad9840
Melker Österberg <melkerosterberg@gmail.com>
2019-09-18 15:51:55 +0200

    Add ListTest.java. It currently does nothing useful

---
+++ python/common/org/python/ListTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ListTest {
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        assertEquals(0, 1);
+    }
+}
+
