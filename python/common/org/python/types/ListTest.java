097005661e3412de892afedf45914526da8350e9
Jonas Norlinder <jonas@norlinder.nu>
2019-09-19 18:01:31 +0200

    Add exception test for __iadd__ and test __iadd__ for lists

--- python/common/org/python/types/ListTest.java
+++ python/common/org/python/types/ListTest.java
@@ -1,5 +1,7 @@
 package org.python.types;
 
+import org.Python;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertFalse;
@@ -25,5 +27,43 @@ public class ListTest {
     public void __hash___shouldThrowException() {
         new List().__hash__();
     }
+
+    @Test(expected = org.python.exceptions.TypeError.class)
+    public void __iadd___exceptionOnInt() {
+        new List().__iadd__(org.python.types.Int.getInt(1));
+    }
+
+    @Test(expected = org.python.exceptions.TypeError.class)
+    public void __iadd___exceptionOnBool() {
+        new List().__iadd__(org.python.types.Bool.getBool(true));
+    }
+
+    @Test(expected = org.python.exceptions.TypeError.class)
+    public void __iadd___exceptionOnEllipsis() {
+        new List().__iadd__(org.python.types.Ellipsis.ELLIPSIS);
+    }
+
+    @Test(expected = org.python.exceptions.TypeError.class)
+    public void __iadd___exceptionOnNone() {
+        new List().__iadd__(org.python.types.NoneType.NONE);
+    }
+
+    @Test()
+    public void __iadd__lists() {
+        List l1 = new List();
+        List l2 = new List();
+
+        l1.append(org.python.types.Int.getInt(1));
+        l2.append(org.python.types.Int.getInt(2));
+
+        l1.__iadd__(l2);
+
+        assertEquals(l1.__getitem__(org.python.types.Int.getInt(0)), org.python.types.Int.getInt(1));
+        assertEquals(l1.__getitem__(org.python.types.Int.getInt(1)), org.python.types.Int.getInt(2));
+        assertEquals(l1.__len__(), org.python.types.Int.getInt(2));
+        assertEquals(l2.__len__(), org.python.types.Int.getInt(1));
+    }
+
+
 }
 
