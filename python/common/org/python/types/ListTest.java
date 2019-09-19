4d18c74d994a0e63385bea8530f549319a9ef8af
Joel <jowe3751@student.uu.se>
2019-09-19 14:04:44 +0200

    Add test for __setitem__ in List tests

--- python/common/org/python/types/ListTest.java
+++ python/common/org/python/types/ListTest.java
@@ -5,10 +5,39 @@ import org.junit.Test;
 
 public class ListTest {
 
-    // Simple stub to test if compilation of tests works
     @Test
-    public void List() {
+    public void testSetItemInteger() {
         org.python.types.List l = new List();
+        l.append(Int.getInt(1));
+        l.__setitem__(Int.getInt(0), Int.getInt(5));
+        assertEquals(Int.getInt(5), l.__getitem__(Int.getInt(0)));
     }
+
+    @Test
+    public void testSetItemString() {
+        org.python.types.List l = new List();
+        l.append(Int.getInt(1));
+        l.append(Int.getInt(2));
+        l.append(Int.getInt(3));
+        l.__setitem__(Int.getInt(1), new Str("hello"));
+        l.__setitem__(Int.getInt(2), new Str("there"));
+        assertEquals(Int.getInt(1), l.__getitem__(Int.getInt(0)));
+        assertEquals(new Str("hello"), l.__getitem__(Int.getInt(1)));
+        assertEquals(new Str("there"), l.__getitem__(Int.getInt(2)));
+    }
+
+    @Test(expected = org.python.exceptions.IndexError.class)
+    public void testSetItemOutOfBound() {
+        org.python.types.List l = new List();
+        l.__setitem__(Int.getInt(0), Int.getInt(5));
+    }
+
+    @Test(expected = org.python.exceptions.IndexError.class)
+    public void testSetItemOutOfBoundsNegativeIndex() {
+        org.python.types.List l = new List();
+        l.append(Int.getInt(1));
+        l.__setitem__(Int.getInt(-2), Int.getInt(5));
+    }
+
 }
 
