68215906e19426fa1776ec18a5cd2697deed1ca8
Melker Österberg <melkerosterberg@gmail.com>
2019-09-19 12:19:19 +0200

    Add comment about test stub

--- python/common/org/python/types/ListTest.java
+++ python/common/org/python/types/ListTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class ListTest {
 
+    // Simple stub to test if compilation of tests works
     @Test
     public void List() {
         org.python.types.List l = new List();
