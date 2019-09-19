8210a1194bdb5b3993375d99b308340db74f4eab
Melker Österberg <melkerosterberg@gmail.com>
2019-09-19 11:15:10 +0200

    Add running test

---
+++ python/common/org/python/types/ListTest.java
@@ -0,0 +1,13 @@
+package org.python.types;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ListTest {
+
+    @Test
+    public void List() {
+        org.python.types.List l = new List();
+    }
+}
+
